4a43802224166128f2a16b3b800747543e405205
sonuarman <mansooriarman090@gmail.com>
2024-08-17 20:51:21 +0530

    ascii idea

---
+++ string/Ascii.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	char ch;
+	cin>>ch;
+	//its a basic concept to check the ascii value of character 
+	//just write the code in int it will take that value as ascii
+	cout<<int(ch)<<" ";
+	//basic use during the various programmes to make it under the 0 to 26 
+	cout<<ch-'A';
+	
+}
