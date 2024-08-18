dd1cf5eefbe7c379de1fa82ed018b4c6acd6f645
sonuarman <mansooriarman090@gmail.com>
2024-08-18 18:14:33 +0530

    double typecastinf concetpt

---
+++ math/area.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//area of circle 
+	//what will be given  in the question 
+	//radius of the circle 
+	int n;
+	cin>>n;
+	//learn the concept here 
+	//as i think that you didnt have learned yet 
+	//whenver you are dealing with the decimal value after the point always use the double concept 
+	//or you can use the type case
+	cout<<double(3.14*n*n);
+}
