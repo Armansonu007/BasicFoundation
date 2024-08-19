df417e35c20c61fdf5a1d092d9c9c497e123f1a0
sonuarman <mansooriarman090@gmail.com>
2024-08-19 14:13:22 +0530

    greater.cpp

---
+++ math/greter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double n;
+    cin >> n;
+    double m;
+    cin>>m;
+    if(n>m){
+        cout<<n;
+    }
+    else cout<<m;
+}    
+    
