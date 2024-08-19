4237b384ee28b0f67666859f6af38d7063c9b538
sonuarman <mansooriarman090@gmail.com>
2024-08-19 11:31:59 +0530

    evenodd

---
+++ math/evenodd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    //check if number is even or odd
+    // 111
+    // 001
+    // ---
+    // 001
+    //when we take the and operator with any number 
+    //it will give one if we take in if ondition it will give true
+    if(n%2){
+        cout<<"odd";
+    }
+    else cout<<"even";
+    
+}
