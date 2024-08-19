1b57817b503b875c5ac9f081299dda0377a2b99b
sonuarman <mansooriarman090@gmail.com>
2024-08-19 12:31:59 +0530

    ap.cpp

---
+++ math/ap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    //in this problem we have three thing that are given in the problem 
+    //that is first one is first term , common differnece and the last terms that is n
+    int d, a;
+    cin>>a>>d;
+    cout<<a+(n-1)*d;
+}    
+    
