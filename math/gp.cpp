6f6e644afce0a481b600be739ff46f5aa9264439
sonuarman <mansooriarman090@gmail.com>
2024-08-19 14:08:15 +0530

    gp

---
+++ math/gp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    //in this problem we have three thing that are given in the problem 
+    //that is first one is first term , common differnece and the last terms that is n
+    double r, a;
+    cin>>a>>r;
+    cout<< a * (pow(r, n) - 1)) / (r - 1);
+}    
+    
