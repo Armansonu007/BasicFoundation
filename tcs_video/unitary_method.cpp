5e1a4b9a10caad1d520f432ca42dc65e09e1d8f9
sonuarman <mansooriarman090@gmail.com>
2024-10-02 14:57:41 +0530

    unitary method

---
+++ tcs_video/unitary_method.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//calcuate the total shipping cost 
+	double b_m=5;
+	double cost_kg=2.0;
+	double cost_km=0.5/10;
+	
+	double weight, dist;
+	cin>>weight>>dist;
+	
+	//toatla answer 
+	//base + cost_kg * weight + cost_km* dist
+	cout<<b_m+ cost_kg* weight + cost_km* dist;
+	
+	      
+}
